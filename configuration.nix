21e2ed4ff7dd625f777c00b243ac2e41188f9954
Root <root@mail.com>
2026-06-19 08:36:14 +0200

    batman

---
+++ configuration.nix
@@ -0,0 +1,133 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "laptop"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  programs.hyprland.enable = true;
+
+  services.spice-vdagentd.enable = true; # TODO: remove this once config is done
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+      	command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
+	user = "greeter";
+      };
+    };
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "eu";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."ntreml" = {
+    isNormalUser = true;
+    description = "Niklas Treml";
+    extraGroups = [ "networkmanager" "wheel" "docker"];
+    shell = pkgs.fish;
+    packages = with pkgs; [ home-manager ];
+  };
+
+  # home-manager.users.ntreml = import ./home.nix;
+
+
+  # Install firefox.
+  programs.fish = {
+    enable = true;
+    # interactiveShellInit = ''
+    #   fenv source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+    # '';
+  };
+  programs.neovim.enable = true;
+  virtualisation.docker.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    jujutsu
+    walker
+    elephant
+    hyprlock
+    hyprpaper
+    waybar
+    tuigreet
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}

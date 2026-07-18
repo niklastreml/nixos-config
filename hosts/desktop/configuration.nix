c5fd000c81ce9af5d55d6f1bbe3773a1025544b3
Niklas Treml <27763017+niklastreml@users.noreply.github.com>
2026-07-18 23:16:03 +0200

    feat: prepare desktop
    Signed-off-by: Niklas Treml <27763017+niklastreml@users.noreply.github.com>

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    ../../modules/common.nix
+    ../../modules/audio.nix
+    ../../modules/window-manager.nix
+    ../../modules/cli.nix
+    ../../modules/gui.nix
+    (import ../../modules/grub-theme.nix {
+      theme = "mojave";
+      type = "float";
+      screen = "2k";
+      color = "dark";
+    })
+    ../../modules/bluetooth.nix
+    ../../modules/steam.nix
+    ../../modules/networkmanager.nix
+    ../../modules/wifi.nix
+    ../../modules/docker.nix
+    ./hardware-configuration.nix
+  ];
+
+  networking.hostName = "desktop";
+  services.libinput.enable = true;
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
+}

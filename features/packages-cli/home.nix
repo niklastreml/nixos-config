6c508491803c6543bfe5eb24e90cd5558b5c32db
Niklas Treml <27763017+niklastreml@users.noreply.github.com>
2026-08-04 21:18:16 +0200

    feat: devenv
    Signed-off-by: Niklas Treml <27763017+niklastreml@users.noreply.github.com>

--- features/packages-cli/home.nix
+++ features/packages-cli/home.nix
@@ -10,47 +10,48 @@ in
 {
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-    autojump
-    bat
-    bear
-    bitwarden-cli
-    chafa
-    cloc
-    curl
-    difftastic
-    dive
-    dnsutils
-    eza
-    file
-    fd
+      autojump
+      bat
+      bear
+      bitwarden-cli
+      chafa
+      cloc
+      curl
+      devenv
+      difftastic
+      dive
+      dnsutils
+      eza
+      file
+      fd
       gh
       ghostscript
       gnumake
-    graphviz
-    helmfile
-    jq
-    kubernetes-helm
-    home-manager
-    kind
-    krew
-    kubectl
-    lua5_1
-    luarocks
-    mercurial
-    mermaid-cli
-    net-tools
-    nh
-    libqalculate
-    ripgrep
-    texliveBasic
-    tree-sitter
-    typst
-    unzip
-    (callPackage ./libtexprintf.nix { })
-    vim
-    nodejs
-    wget
-  ];
+      graphviz
+      helmfile
+      jq
+      kubernetes-helm
+      home-manager
+      kind
+      krew
+      kubectl
+      lua5_1
+      luarocks
+      mercurial
+      mermaid-cli
+      net-tools
+      nh
+      libqalculate
+      ripgrep
+      texliveBasic
+      tree-sitter
+      typst
+      unzip
+      (callPackage ./libtexprintf.nix { })
+      vim
+      nodejs
+      wget
+    ];
 
     # CLI tools whose only config is enabling the home-manager module (which
     # also wires up stylix theming and shell integration).

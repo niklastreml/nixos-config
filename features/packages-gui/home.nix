cb830671959ecc384df52a15d70da758346ed535
Niklas Treml <27763017+niklastreml@users.noreply.github.com>
2026-08-04 17:27:06 +0200

    fix: pointer cursor
    Signed-off-by: Niklas Treml <27763017+niklastreml@users.noreply.github.com>

--- features/packages-gui/home.nix
+++ features/packages-gui/home.nix
@@ -1,9 +1,15 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.myFeatures.packages-gui;
 in
 {
   config = lib.mkIf cfg.enable {
+    home.pointerCursor.enable = true;
     home.packages = with pkgs; [
       brave
       vesktop
